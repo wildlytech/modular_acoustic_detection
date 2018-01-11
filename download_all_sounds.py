ebdbf7c66cf2ec52561f6d37315f53c4ad6ca8b6
Nandan Sridhar <nandan.sridhar@gmail.com>
2018-01-11 15:19:01 -0800

    added script to quickly download_all_sounds added script to install all necessary packages for ubuntu

---
+++ download_all_sounds.py
@@ -0,0 +1,4 @@
+from youtube_audioset import download_data
+
+if __name__ == "__main__":
+	download_data()
---
+++ ubuntu_packages_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython python-pip python-dev python-setuptools ffmpeg
+sudo pip install --upgrade pip
+sudo pip install --upgrade pandas
+sudo pip install pydub peakutils librosa
+sudo pip install matplotlib seaborn statsmodels sqlalchemy
+sudo pip install --upgrade youtube_dl
+sudo pip install scikit-learn xgboost xgbfir
