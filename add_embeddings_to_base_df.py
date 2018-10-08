6af7dd9f23a79831683d7334f0975682b9088c00
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-08 14:42:26 +0530

    this adds a column with embeddings of each file to base dataframe

--- add_embeddings_to_base_df.py
+++ add_embeddings_to_base_df.py
@@ -3,33 +3,38 @@ import numpy
 import pickle
 import glob
 
+#give the path where the embeddings of the downloaded files are saved
+path_where_embedddings_saved = 'data/audioset/explosion/'
 
-#Load the unbalanced_data.pkl file
-with open('unbalanced_data.pkl','rb') as f:
+#Load the downloaded base dataframe generated from download_data() function of youtube_audioset
+with open('downloaded_base_dataframe.pkl','rb') as f:
     un_df=pickle.load(f)
 
-#Glob all the generated pickle files( embeddings ) from the generting_embeddings.py
-pickle_files = glob.glob('<path where the embeddings are written>/*.pkl')
-
+# glob all the generated pickle files( embeddings ) from the generting_embeddings.py
+embedding_files = glob.glob(path_where_embedddings_saved + '*.pkl')
+#get the YTID from embedding files names
+ytid =[]
+for each_file_name in embedding_files:
+    ytid.append(each_file_name.split('/')[-1][:11])
 
 #Reading all the pickle files into the python environment
 embeddings=[]
-for pkl in pickle_files:
-    with open(pkl,'rb') as f:
+for embedding_each_file in embedding_files:
+    with open(embedding_each_file,'rb') as f:
         arb=pickle.load(f)
     embeddings.append(arb)
     arb=[]
 
 #Creating a datafrane with features ( embeddings ) as column and then merging with the base DataFrame.
 features_df=pd.DataFrame()
-features_df['pkl_files']=pickle_files
-fetaures_df['features']=embeddings
+features_df['YTID'] = ytid
+features_df['features'] = embeddings
 
 #Merge the base dataframe with the intermediate dataframe to get the final unbalanced dataframe with feature as column
-un_df['pkl_files'] = un_df['YTID'].astype(str)+'-'+un_df['start_seconds'].apply(int).astype(str)+'.pkl'
-final_df=pd.merge(un_df,fetaures_df,on='pkl_files')
-del final_df['pkl_files']
+final_df = pd.merge(un_df,features_df,on='YTID')
 
 #Now overwrite the unbalanced_data.pkl file with included features column
-with open('unbalanced_data.pkl','w') as f:
+with open('downloaded_final_dataframe.pkl','w') as f:
     pickle.dump(final_df,f)
+
+print 'Final data frame with feature column is saved as  " downloaded_final_dataframe.pkl "..!!'
