b2977ef331098ea34d875d5a9d2a782b42a390ff
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-08 15:52:40 +0530

    added function to balance the data

--- model_training.py
+++ model_training.py
@@ -38,14 +38,13 @@ human_sounds = get_recursive_sound_names(human_sounds)
 nature_sounds = get_recursive_sound_names(nature_sounds)
 
 #Read the balanced data created by running the balancing_datasets.py
-with open('balanced_data.pkl','rb') as f:
-    df=pickle.load(f)
-print(df.shape)
-df['labels']=df['labels_name']
+
+#Note that this is binary classification. Balancing must be  [ Ambient ] vs  [ Impact ]
+df = balancing_dataset.balanced_data()
 
 # Binarize the labels
 name_bin = LabelBinarizer().fit(ambient_sounds + impact_sounds)
-labels_split = df['labels'].apply(pd.Series).fillna('None')
+labels_split = df['labels_name'].apply(pd.Series).fillna('None')
 labels_binarized = name_bin.transform(labels_split[labels_split.columns[0]])
 for column in labels_split.columns:
     labels_binarized |= name_bin.transform(labels_split[column])
@@ -54,16 +53,19 @@ labels_binarized = pd.DataFrame(labels_binarized, columns = name_bin.classes_)
 print(labels_binarized.shape)
 print df.shape[0], "examples"
 
+# print the percentage of Impact and Ambinet sounds
 print "Percentage Impact Sounds:", (labels_binarized[impact_sounds].sum(axis=1) > 0).mean()
 print "Percentage Ambient Sounds:", (labels_binarized[ambient_sounds].sum(axis=1) > 0).mean()
 
 print(labels_binarized.mean())
 
 
+#Filter out the sounds that are having 10 seconds duration.
 df_filtered = df.loc[df.features.apply(lambda x: x.shape[0] == 10)]
-# df_filtered = df.loc[df['labels'].apply(lambda x: (len(x) == 1)) & df.features.apply(lambda x: x.shape[0] == 10)]
 labels_filtered = labels_binarized.loc[df_filtered.index,:]
 
+
+#split the data into train and test
 df_train, df_test, labels_binarized_train, labels_binarized_test = train_test_split(df_filtered, labels_filtered,
                                                                       test_size=0.33, random_state=42,
                                                                       stratify=labels_filtered.any(axis=1)*1)
@@ -77,6 +79,7 @@ y_train = (labels_binarized_train[impact_sounds].any(axis=1)*1).values
 y_test = (labels_binarized_test[impact_sounds].any(axis=1)*1).values
 
 
+# Print the percentage of each sounds in whole data
 print labels_filtered.loc[:,explosion_sounds].any(axis=1).mean()
 print labels_filtered.loc[:,motor_sounds].any(axis=1).mean()
 print labels_filtered.loc[:,wood_sounds].any(axis=1).mean()
@@ -86,24 +89,25 @@ print labels_filtered.loc[:,nature_sounds].any(axis=1).mean()
 
 labels_filtered.loc[:,impact_sounds].any(axis=1).mean()
 
-clf1_ = LogisticRegression(max_iter=1000)
 
-# clf1_train = X_train.reshape((-1,10,128)).max(axis=1)
-# clf1_test = X_test.reshape((-1,10,128)).max(axis=1)
+# Try experimenting with Logistic regression algorithm
+clf1_ = LogisticRegression(max_iter=1000)
 clf1_train = X_train
 clf1_test = X_test
 
-# clf1_train_target = labels_binarized_train.loc[:,motor_sounds].any(axis=1)
-# clf1_test_target = labels_binarized_test.loc[:,motor_sounds].any(axis=1)
+# Assign the Impact sounds( target sounds) as  1's  and ambient sounds as 0's
 clf1_train_target = labels_binarized_train.loc[:,impact_sounds].any(axis=1)
 clf1_test_target = labels_binarized_test.loc[:,impact_sounds].any(axis=1)
 
+#fit the train data 
 clf1_.fit(clf1_train, clf1_train_target)
 
+# Predict on the trained model
 clf1_train_prediction = clf1_.predict(clf1_train)
 clf1_test_prediction = clf1_.predict(clf1_test)
 clf1_test_prediction_prob = clf1_.predict_proba(clf1_test)[:,1]
 
+# Print out the confusion matrix for Train data
 clf1_conf_train_mat = pd.crosstab(clf1_train_target, clf1_train_prediction, margins=True)
 print('Train precsion and recall for Logistic regression')
 print('=============================================')
@@ -115,6 +119,7 @@ print "Train Accuracy:", (clf1_train_prediction == clf1_train_target).mean()
 
 print(clf1_conf_train_mat)
 
+# Print out the confusion matrix for test data
 clf1_conf_test_mat = pd.crosstab(clf1_test_target, clf1_test_prediction, margins=True)
 print('Test precsion and recall for Logistic regression')
 print('=============================================')
@@ -126,6 +131,7 @@ print "Test Accuracy:", (clf1_test_prediction == clf1_test_target).mean()
 print(clf1_conf_test_mat)
 
 
+# create the keras neural netwrok model
 def create_keras_model():
     # create model
     model = Sequential()
@@ -140,23 +146,28 @@ def create_keras_model():
     model.compile(loss='binary_crossentropy', optimizer=RMSprop(lr=5e-4), metrics=['accuracy'])
     return model
 
+# Assign the train and test data and reshape so as to align it to neural network model's input. 
 clf2_train = X_train.reshape((-1,1280,1))
 clf2_test = X_test.reshape((-1,1280,1))
 clf2_train_target = labels_binarized_train.loc[:,impact_sounds].any(axis=1)
 clf2_test_target = labels_binarized_test.loc[:,impact_sounds].any(axis=1)
+
+# call the model and start training 
 model = create_keras_model()
 model_traing=model.fit(clf2_train, clf2_train_target,
           epochs=500, batch_size=5000, verbose=False,
           validation_data = (clf2_test, clf2_test_target))
 
+# Predict on test and train data using the tranined weights
 clf2_train_prediction = model.predict(clf2_train).ravel().round()
 clf2_test_prediction = model.predict(clf2_test).ravel().round()
 clf2_test_prediction_prob = model.predict(clf2_test).ravel()
 
-
+# Accuracy of Train and test 
 print "Train Accuracy:", (clf2_train_prediction == clf2_train_target).mean()
 print "Test Accuracy:", (clf2_test_prediction == clf2_test_target).mean()
 
+#print out the confusion matrix for train data
 clf2_conf_train_mat = pd.crosstab(clf2_train_target, clf2_train_prediction, margins=True)
 print("Training Precision and recall for Keras model")
 print('=============================================')
@@ -166,6 +177,8 @@ print "Train Accuracy:", (clf2_train_prediction == clf2_train_target).mean()
 
 print(clf2_conf_train_mat)
 
+
+#print out the confusion matrix for test data
 clf2_conf_test_mat = pd.crosstab(clf2_test_target, clf2_test_prediction, margins=True)
 print("Testing Precision and recall for Keras model")
 print('=============================================')
