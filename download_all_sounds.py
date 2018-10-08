72952d0f0d4b9a1a1c5c685a84a5565f71e717e2
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-08 15:32:11 +0530

    inputting all the sounds and setting the path for downloaded files

--- download_all_sounds.py
+++ download_all_sounds.py
@@ -1,4 +1,11 @@
 from youtube_audioset import download_data
 
 if __name__ == "__main__":
-    download_data()
+
+	#specify the target sounds to download and set the path for downlaoded files to store 
+	target_sounds = youtube_audioset.ambient_sounds + youtube_audioset.impact_sounds 
+	target_path = 'sounds/ambient_impact/'
+
+	#start downloading all the files 
+	#It will also return a base dataframe with [ YTID, start_seconds, end_seconds, positive_labels, labels_name, wav_file ] name  as columns along with audio wav files 
+    download_data(target_sounds, target_path)
