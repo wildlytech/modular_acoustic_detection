c281f5a80a547f207e2509c024d69bbb829ae183
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-24 13:05:54 +0530

    followed standard coding using pylint

--- add_embeddings_to_base_df.py
+++ add_embeddings_to_base_df.py
@@ -1,44 +1,45 @@
-import pandas as pd
-import numpy
-import pickle
 import glob
 import argparse
+import pickle
+import pandas as pd
 
 #parse the input path where the embeddings of the downloaded files are saved
-parser= argparse.ArgumentParser(description = 'Input the path where embeddings of the downloaded files are saved ')
-parser.add_argument('-path_to_embeddings','--path_where_embedddings_saved',action ='store'  ,help='Input the path')
-result = parser.parse_args()
+PARSER = argparse.ArgumentParser(description='Input the path for embeddings of the downloaded files')
+PARSER.add_argument('-path_to_embeddings', '--path_where_embedddings_saved', action='store', help='Input the path')
+RESULT = PARSER.parse_args()
 
 #Load the downloaded base dataframe generated from download_data() function of youtube_audioset
-with open('downloaded_base_dataframe.pkl','rb') as f:
-    un_df=pickle.load(f)
+with open('downloaded_base_dataframe.pkl', 'rb') as f:
+    UN_DF = pickle.load(f)
 
 # glob all the generated pickle files( embeddings ) from the generting_embeddings.py
-embedding_files = glob.glob(result.path_where_embedddings_saved + '*.pkl')
+EMBEDDINGS_FILES = glob.glob(RESULT.path_where_embedddings_saved + '*.pkl')
 #get the YTID from embedding files names
-ytid =[]
-for each_file_name in embedding_files:
-    ytid.append(each_file_name.split('/')[-1][:11])
+YTID = []
+for each_file_name in EMBEDDINGS_FILES:
+    YTID.append(each_file_name.split('/')[-1][:11])
 
 #Reading all the pickle files into the python environment
-embeddings=[]
+EMBEDDINGS = []
 print 'reading the embedding files..'
-for embedding_each_file in embedding_files:
-    with open(embedding_each_file,'rb') as f:
-        arb=pickle.load(f)
-    embeddings.append(arb)
-    arb=[]
+for embedding_each_file in EMBEDDINGS_FILES:
+    with open(embedding_each_file, 'rb') as f:
+        arb = pickle.load(f)
+    EMBEDDINGS.append(arb)
+    arb = []
 
-#Creating a datafrane with features ( embeddings ) as column and then merging with the base DataFrame.
-features_df=pd.DataFrame()
-features_df['YTID'] = ytid
-features_df['features'] = embeddings
+#Creating a datafrane with features (embeddings) as column and then merge with the base DataFrame.
+FEATURES_DF = pd.DataFrame()
+FEATURES_DF['YTID'] = YTID
+FEATURES_DF['features'] = EMBEDDINGS
 
-#Merge the base dataframe with the intermediate dataframe to get the final unbalanced dataframe with feature as column
-final_df = pd.merge(un_df,features_df,on='YTID')
+#Merge the base dataframe with the intermediate dataframe.
+#To get the final unbalanced dataframe with feature as column
+
+FINAL_DF = pd.merge(UN_DF, FEATURES_DF, on='YTID')
 
 #Now overwrite the unbalanced_data.pkl file with included features column
-with open('downloaded_final_dataframe.pkl','w') as f:
-    pickle.dump(final_df,f)
+with open('downloaded_final_dataframe.pkl', 'w') as f:
+    pickle.dump(FINAL_DF, f)
 
 print 'Final data frame with feature column is saved as  " downloaded_final_dataframe.pkl "..!!'
