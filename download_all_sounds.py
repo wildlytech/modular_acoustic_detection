6f10d64cf67c23fd993c047ca580fe9e8f8dd066
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-25 11:34:50 +0530

    improved read-ability

--- download_all_sounds.py
+++ download_all_sounds.py
@@ -7,5 +7,7 @@ if __name__ == "__main__":
     TARGET_PATH = 'sounds/ambient_impact/'
 
 	#start downloading all the files
-	#It will also return a base dataframe with [ YTID, start_seconds, end_seconds, positive_labels, labels_name, wav_file ] name  as columns along with audio wav files
+	#It will also return a base dataframe with
+	#[ YTID, start_seconds, end_seconds, positive_labels, labels_name, wav_file
+	# name  as columns along with audio wav files
     download_data(TARGET_SOUNDS, TARGET_PATH)
