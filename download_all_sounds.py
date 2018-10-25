a94c0bf06f6be8a1d9f97a78402e9b9d17976ca7
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-25 17:56:55 +0530

    added docstring to the function

--- download_all_sounds.py
+++ download_all_sounds.py
@@ -1,3 +1,6 @@
+"""
+Downloads all the Impact and Ambient sounds
+"""
 import youtube_audioset
 from youtube_audioset import download_data
 
@@ -6,8 +9,8 @@ if __name__ == "__main__":
     TARGET_SOUNDS = youtube_audioset.AMBIENT_SOUNDS + youtube_audioset.IMPACT_SOUNDS
     TARGET_PATH = 'sounds/ambient_impact/'
 
-	#start downloading all the files
-	#It will also return a base dataframe with
-	#[ YTID, start_seconds, end_seconds, positive_labels, labels_name, wav_file
-	# name  as columns along with audio wav files
+    #start downloading all the files
+    #It will also return a base dataframe with
+    #[ YTID, start_seconds, end_seconds, positive_labels, labels_name, wav_file
+    # name  as columns along with audio wav files
     download_data(TARGET_SOUNDS, TARGET_PATH)
