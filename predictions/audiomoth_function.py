3e5176a3ee98c208e5a8057c6032156e779bbf30
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-26 11:49:31 +0530

    removed unncessary spaces

--- predictions/audiomoth_function.py
+++ predictions/audiomoth_function.py
@@ -3,12 +3,10 @@ Retuns the audiomoth files as a dataframe format
 with 'feature' as column and this is furthur used
 to predict the sounds using pre-trained model
 """
-
 import pickle
 import glob
 import pandas as pd
 from pydub import AudioSegment
-
 
 def audio_dataframe(path_to_audio_files):
     """
@@ -39,8 +37,6 @@ def audio_dataframe(path_to_audio_files):
     data_frame = pd.DataFrame()
     data_frame['audiomoth_id'] = audiomoth_id
     data_frame['wav_file'] = wave_files
-
-
     # Merge the dataframe to get the required dataframe
     req = pd.merge(labels_csv, data_frame, on='wav_file')
     data_frame = req
@@ -53,20 +49,15 @@ def embeddings_on_dataframe(path_to_audio_files, path_to_embeddings):
     Returns the dataframe with 'feature' as column of those
     that are annotated
     """
-
     data_frame = audio_dataframe(path_to_audio_files)
-
     # Get all the pickle files into list
     pickle_files = glob.glob(path_to_embeddings+'*.pkl')
-
     # create a list of audiomoth file name and its id's
     audiomoth_id = []
     for i in pickle_files:
         audiomoth_id.append((i.split('/')[-1]) [:-4])
-
     #Read all the embeddings
     clf1_test = []
-
     for i in pickle_files:
         try:
             with open(i, 'rb') as file_obj:
@@ -74,7 +65,6 @@ def embeddings_on_dataframe(path_to_audio_files, path_to_embeddings):
             clf1_test.append(arb_wav)
         except:
             print 'Test Pickling Error'
-
     # create a dataframe with embeddings as column
     emb_df = pd.DataFrame()
     emb_df['audiomoth_id'] = audiomoth_id
@@ -88,15 +78,11 @@ def embeddings_without_annotation(path_to_embeddings):
     Returns dataframe with 'feature' as column as those
     that are not annotated
     """
-
     pickle_files = glob.glob(path_to_embeddings+'*.pkl')
-
     audiomoth_id = []
     for i in pickle_files:
         audiomoth_id.append((i.split('/')[-1])[:-4])
-
     clf1_test = []
-
     for i in pickle_files:
         try:
             with open(i, 'rb') as file_obj:
@@ -104,13 +90,12 @@ def embeddings_without_annotation(path_to_embeddings):
             clf1_test.append(arb_wav)
         except:
             print 'pickle read ERROR'
-
     #create a dataframe with embeddings as column
     emb_df = pd.DataFrame()
     emb_df['audiomoth_id'] = audiomoth_id
     emb_df['features'] = clf1_test
-
     #print the head and shape of the datframe
     print emb_df.head()
     print 'shape :', emb_df.shape
+
     return emb_df
