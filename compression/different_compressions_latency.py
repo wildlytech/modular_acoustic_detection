0f7ed832fcc74aae3d7aa61fe1264017dc2d2caf
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-26 13:54:45 +0530

    changed to standarad global constant naming format

--- compression/different_compressions_latency.py
+++ compression/different_compressions_latency.py
@@ -1,26 +1,24 @@
-import numpy as np
-import pandas as pd
-import pickle
-import pydub
-from pydub import AudioSegment
+"""
+Different audio codecs are used to measure the latency
+"""
 import time
 import subprocess
 
-start=time.time()
+START = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a aac -b:a 64k  speech.aac', shell=True)
-print 'Timing for aac :', (time.time() - start)
-start1=time.time()
+print 'Timing for aac :', (time.time() - START)
+START1 = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a ac3 -b:a 64k  speech.ac3', shell=True)
-print 'Timing for ac3 :', (time.time() -start1)
-start2=time.time()
+print 'Timing for ac3 :', (time.time() -START1)
+START2 = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a mp2 -b:a 64k  speech.mp2', shell=True)
-print 'Timing for mp2 :', (time.time() -start2)
-start3=time.time()
+print 'Timing for mp2 :', (time.time() -START2)
+START3 = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a flac -b:a 64k  speech.flac', shell=True)
-print 'Timing for flac :', (time.time() - start3)
-start4=time.time()
+print 'Timing for flac :', (time.time() - START3)
+START4 = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a libopus -b:a 64k  speech.opus', shell=True)
-print 'Timing for libopus :', (time.time() - start4)
-start5=time.time()
+print 'Timing for libopus :', (time.time() - START4)
+START5 = time.time()
 subprocess.call('ffmpeg -i speech.wav  -c:a mp3 -b:a 64k  speech.mp3', shell=True)
-print 'Timing for mp3 :', (time.time() - start5)
+print 'Timing for mp3 :', (time.time() - START5)
