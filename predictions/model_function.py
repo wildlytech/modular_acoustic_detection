f5d44b329be8a1f0537f6e19beb97f7f931f090e
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-26 12:13:18 +0530

    added docstring and removed extra spaces

--- predictions/model_function.py
+++ predictions/model_function.py
@@ -1,39 +1,46 @@
-import pandas as pd
-import keras
+"""
+Predictions are returned for a single wav file
+"""
+
 from keras.models import Sequential
-from keras.layers import Dense, Conv1D, Conv2D, MaxPooling1D, Flatten, AveragePooling1D
-from keras.layers.core import Lambda
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.optimizers import RMSprop, Adam
-import numpy as np
-import argparse
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten
+from keras.optimizers import Adam
 
 
-#Define the model
 def create_keras_model():
-    # create model
+    """
+    Create a model
+    """
     model = Sequential()
-    model.add(Conv1D(500, input_shape=(1280,1), kernel_size=128, strides=128, activation='relu', padding='same'))
+    model.add(Conv1D(500, input_shape=(1280, 1),
+                     kernel_size=128,
+                     strides=128,
+                     activation='relu',
+                     padding='same'))
     model.add(Dense(500, activation='relu'))
-    model.add(Dense(500,activation='relu'))
+    model.add(Dense(500, activation='relu'))
     model.add(Dense(500, activation='relu'))
     model.add(MaxPooling1D(10))
     model.add(Dense(6, activation='sigmoid'))
     model.add(Flatten())
     print model.summary()
     # Compile model
-    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=1e-4,epsilon=1e-8), metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=Adam(lr=1e-4, epsilon=1e-8),
+                  metrics=['accuracy'])
+
     return model
 
 def predictions_wavfile(data):
-
+    """
+    Takes the embeddings of wav file and runs the predictions on it
+    """
     # call the model and load the weights
     model = create_keras_model()
     model.load_weights('multiclass_weights.model')
-    test_data = data.reshape((-1,1280,1))
-
+    test_data = data.reshape((-1, 1280, 1))
     #predict the data using the loaded model
-    predictions_prob= model.predict(test_data).ravel()
+    predictions_prob = model.predict(test_data).ravel()
     predictions = predictions_prob.round()
 
     return predictions_prob, predictions
