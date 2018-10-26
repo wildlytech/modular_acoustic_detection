d3d3c38c72e9737a16ca9b2c56d585bd8e7a5a9a
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-10-26 11:46:38 +0530

    added function to predict audiomoth files that are not annotated

--- predictions/audiomoth_function.py
+++ predictions/audiomoth_function.py
@@ -1,17 +1,19 @@
+"""
+Retuns the audiomoth files as a dataframe format
+with 'feature' as column and this is furthur used
+to predict the sounds using pre-trained model
+"""
+
 import pickle
 import glob
-sys.path.insert(0, '../')
-import youtube_audioset
 import pandas as pd
-import numpy as np
-import pydub
 from pydub import AudioSegment
 
 
-#for reading all the audiomoth (.WAV ) files and creating a dataframe
 def audio_dataframe(path_to_audio_files):
-
-    #Read the annotated file
+    """
+    Reads the annotated file in .csv file
+    """
     labels_csv = pd.read_csv('Nisarg _annotation - Eravikulam.csv')
 
     # Glob all the audio files ( .WAV) that are of audiomoth recording
@@ -21,61 +23,94 @@ def audio_dataframe(path_to_audio_files):
     #check for audio duration to be 10 seconds. Remove if its not.
     for i in audiomoth_wav_files:
         sound = AudioSegment.from_wav(i)
-        if not len(sound)==10000:
+        if len(sound) != 10000:
             print i + 'is removed, as it is not a ten second audio'
             audiomoth_wav_files.remove(i)
 
     # create lists that has audiomoth file name and id's
     audiomoth_id = []
-    wave_files= []
+    wave_files = []
     for i in audiomoth_wav_files:
         audiomoth_id.append((i.split('/')[-1])[:-4])
         wave_files.append(i.split('/')[-1])
     print 'first element :', audiomoth_id[0]
 
     # create a dataframe
-    df = pd.DataFrame()
-    df['audiomoth_id'] = audiomoth_id
-    df['wav_file'] = wave_files
+    data_frame = pd.DataFrame()
+    data_frame['audiomoth_id'] = audiomoth_id
+    data_frame['wav_file'] = wave_files
 
 
     # Merge the dataframe to get the required dataframe
-    req = pd.merge(labels_csv,df,on='wav_file')
-    df=req
+    req = pd.merge(labels_csv, data_frame, on='wav_file')
+    data_frame = req
 
-    return df
+    return data_frame
 
-#reading all the embeddings
-def embeddings_on_dataframe(path_to_audio_files,path_to_embeddings):
 
-    df = audio_dataframe(path_to_audio_files, path_to_embeddings)
+def embeddings_on_dataframe(path_to_audio_files, path_to_embeddings):
+    """
+    Returns the dataframe with 'feature' as column of those
+    that are annotated
+    """
 
-    # Get all the pickle files into list 
+    data_frame = audio_dataframe(path_to_audio_files)
+
+    # Get all the pickle files into list
     pickle_files = glob.glob(path_to_embeddings+'*.pkl')
 
     # create a list of audiomoth file name and its id's
-    audiomoth_id=[]
+    audiomoth_id = []
     for i in pickle_files:
-        audiomoth_id.append( ( i.split('/')[-1]) [:-4])
+        audiomoth_id.append((i.split('/')[-1]) [:-4])
 
     #Read all the embeddings
-    clf1_test=[]
+    clf1_test = []
 
     for i in pickle_files:
         try:
-            with open(i,'rb') as f:
-                arb_wav = pickle.load(f)
+            with open(i, 'rb') as file_obj:
+                arb_wav = pickle.load(file_obj)
             clf1_test.append(arb_wav)
-        except :
+        except:
             print 'Test Pickling Error'
 
     # create a dataframe with embeddings as column
-    emb_df= pd.DataFrame()
+    emb_df = pd.DataFrame()
     emb_df['audiomoth_id'] = audiomoth_id
     emb_df['features'] = clf1_test
-    final = pd.merge(df,emb_df,on='audiomoth_id')
-
+    final = pd.merge(data_frame, emb_df, on='audiomoth_id')
 
     return final
 
-    
+def embeddings_without_annotation(path_to_embeddings):
+    """
+    Returns dataframe with 'feature' as column as those
+    that are not annotated
+    """
+
+    pickle_files = glob.glob(path_to_embeddings+'*.pkl')
+
+    audiomoth_id = []
+    for i in pickle_files:
+        audiomoth_id.append((i.split('/')[-1])[:-4])
+
+    clf1_test = []
+
+    for i in pickle_files:
+        try:
+            with open(i, 'rb') as file_obj:
+                arb_wav = pickle.load(file_obj)
+            clf1_test.append(arb_wav)
+        except:
+            print 'pickle read ERROR'
+
+    #create a dataframe with embeddings as column
+    emb_df = pd.DataFrame()
+    emb_df['audiomoth_id'] = audiomoth_id
+    emb_df['features'] = clf1_test
+
+    #print the head and shape of the datframe
+    print emb_df.head()
+    print 'shape :', emb_df.shape
+    return emb_df
