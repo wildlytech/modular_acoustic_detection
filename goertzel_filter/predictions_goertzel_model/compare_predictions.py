9968dcbbbffc320acbf4746349e29f7ebaf142e2
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-12-03 12:16:22 +0530

    compares the prediction results made by libraries and without using libraries

--- goertzel_filter/predictions_goertzel_model/compare_predictions.py
+++ goertzel_filter/predictions_goertzel_model/compare_predictions.py
@@ -1,46 +1,68 @@
+"""
+Compares the predictions made using model libaries and without libraries
+"""
+import argparse
 import keras
 from keras.models import Sequential, Model
 from keras.layers import Input
-import pickle
 import predicting_without_libraries
 from keras.layers import Dense, Conv1D, Conv2D, MaxPooling1D, Flatten, AveragePooling1D, TimeDistributed, MaxPooling2D
 import audiomoth_function_for_goertzel_model
 
 
-#give the path where the frequency components are saved ( '.pkl' files )
-path_to_goertzel_components = '/media/wildly/1TB-HDD/goertzel_data_ervikulam_8k/'
-audio_files_path = '/media/wildly/1TB-HDD/AudioMoth/test_wave/'
+# Define the constants
+DESCRIPTION = "Compares the prediction my goertzel model and annotated labels"
+HELP_AUDIO = "Path for audio files ( .WAV )"
+HELP_GOERTZEL = "Path for Goertzel freq components ( .pkl )"
 
-inputs = Input(shape=(10,8000,4))
-conv_1= TimeDistributed(Conv1D(100, kernel_size=200,strides=100,activation='relu',padding='same'),input_shape=(10,8000,4))(inputs)
-conv_2 = TimeDistributed(Conv1D(100, kernel_size=4,activation='relu',padding='same'))(conv_1)
-max_pool = TimeDistributed(MaxPooling1D(80))(conv_2)
-dense_1 = TimeDistributed(Dense(60, activation = 'relu'))(max_pool)
-dense_2 = TimeDistributed(Dense(50, activation = 'relu'))(dense_1)
-dense_3 = TimeDistributed(Dense(1, activation='sigmoid'))(dense_2)
-max_pool_2 = MaxPooling2D((10,1))(dense_3)
-predictions = Flatten()(max_pool_2)
+
+#parse the input arguments given from command line
+PARSER = argparse.ArgumentParser(description=DESCRIPTION)
+PARSER.add_argument('-audio_files_path',
+                    '--audio_files_path', action='store',
+                    help=HELP_AUDIO)
+PARSER.add_argument('-path_for_goertzel_components',
+                    '--path_for_goertzel_components', action='store',
+                    help=HELP_GOERTZEL)
+RESULT = PARSER.parse_args()
+
+
+# Define the model architecture using keras functional API
+INPUTS = Input(shape=(10, 8000, 4))
+CONV_1 = TimeDistributed(Conv1D(100,
+								kernel_size=200,
+								strides=100,
+								activation='relu',
+								padding='same'),
+						input_shape=(10, 8000, 4))(INPUTS)
+CONV_2 = TimeDistributed(Conv1D(100, kernel_size=4, activation='relu', padding='same'))(CONV_1)
+MAX_POOL = TimeDistributed(MaxPooling1D(80))(CONV_2)
+DENSE_1 = TimeDistributed(Dense(60, activation='relu'))(MAX_POOL)
+DENSE_2 = TimeDistributed(Dense(50, activation='relu'))(DENSE_1)
+DENSE_3 = TimeDistributed(Dense(1, activation='sigmoid'))(DENSE_2)
+MAX_POOL_2 = MaxPooling2D((10, 1))(DENSE_3)
+PREDICTIONS = Flatten()(MAX_POOL_2)
 # print conv_1.output
-model = Model(inputs=[inputs],outputs=[predictions])
-print model.summary()
+MODEL = Model(inputs=[INPUTS], outputs=[PREDICTIONS])
+print MODEL.summary()
 
 #Load weights from saved weights file
-model.load_weights('Goertzel_model_8k_weights_time.h5')
+MODEL.load_weights('Goertzel_model_8k_weights_time.h5')
 
 # get the weights of each layer from loaded model
-weights =[]
-for layer in model.layers:
-	weights.append(layer.get_weights())
-print 'Length of  the weights : ', len(weights)
+WEIGHTS = []
+for layer in MODEL.layers:
+    WEIGHTS.append(layer.get_weights())
+print 'Length of  the weights : ', len(WEIGHTS)
 
 #redict on audiomoth files using keras library
-import audiomoth_function_for_goertzel_model
-test_values, df = audiomoth_function_for_goertzel_model.dataframe_with_frequency_components(audio_files_path, path_to_goertzel_components)
-predictions = model.predict(test_values).ravel()
-df['predictions_prob'] = predictions
-df['predictions'] = predictions.ravel().round()
+TEST_VAULES, DATA_FRAME = audiomoth_function_for_goertzel_model.dataframe_with_frequency_components(RESULT.audio_files_path,RESULT.path_to_goertzel_components)
+PREDICTIONS_OUT = MODEL.predict(TEST_VAULES).ravel()
+DATA_FRAME['predictions_prob'] = PREDICTIONS_OUT
+DATA_FRAME['predictions'] = PREDICTIONS_OUT.ravel().round()
 
 #predict on any of the audiomoth file without using any of the keras library
 #In this example code we are taking 100th example
-ouput = predicting_without_libraries.layer1_predictions( test_values[100], weights )
-print df['predictions_prob'][100]
+OUTPUT = predicting_without_libraries.layer1_predictions(TEST_VAULES[100], WEIGHTS)
+print OUTPUT
+print DATA_FRAME['predictions_prob'][100]
