42d5f1d22203264db91562ab5b7bfa9d36284bd2
ShivarajMeti <shivaraj.meti002@gmail.com>
2018-12-07 15:21:24 +0530

    updating the packages list that are to be installed

--- ubuntu_packages_install.sh
+++ ubuntu_packages_install.sh
@@ -7,3 +7,10 @@ sudo pip install pydub peakutils librosa
 sudo pip install matplotlib seaborn statsmodels sqlalchemy
 sudo pip install --upgrade youtube_dl
 sudo pip install scikit-learn xgboost xgbfir
+sudo pip install keras
+sudo pip install tensorflow
+sudo pip install dash==0.31.1  
+sudo pip install dash-html-components==0.13.2  
+sudo pip install dash-core-components==0.39.0 
+sudo pip install argparse
+sudo pip install resampy 
