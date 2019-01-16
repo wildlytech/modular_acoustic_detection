512c8aac40a1976a3648e174d6405976c768affe
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-01-16 12:17:40 +0530

    changes made to avoid the incomplete downloading of files

--- Dash_integration/ftp_test.py
+++ Dash_integration/ftp_test.py
@@ -11,18 +11,25 @@ def ftp_data():
     connect to the remote ftp
     """
     ftp = FTP('ftp.dlptest.com', user='dlpuser@dlptest.com', passwd='e73jzTRTNqCN9PYAAjjn')
-    ftp.cwd('/wav_file/')
-    ftp.pwd()
+    # ftp.cwd('/wav_file/')
+    # ftp.pwd()
     ex = ftp.nlst()
-    with open('list_of_files.pkl', 'rb') as file_obj:
-        old_ex = pickle.load(file_obj)
+    with open('downloaded_from_ftp.pkl', 'rb') as file_obj:
+        downloaded_files = pickle.load(file_obj)
+    EX = []
+    for wav in ex:
+        if len(wav) > 3 and ((wav[-3:] == 'wav') or (wav[-3:] == 'WAV')):
+            if (ftp.size(wav))/1000 > 870:
+            # print wav
+                EX.append(wav)
+    print "curated list:", EX
 
     #compare for the length of old number of files.
-    if len(ex) > len(old_ex):
-        ftp.quit()
-        return (len(ex) - len(old_ex)), 1
+    if len(EX) > len(downloaded_files):
+        ftp.close()
+        return (len(EX) - len(downloaded_files)), 1
     else:
-        ftp.quit()
+        ftp.close()
         return 0, 0
 
 def get_files():
@@ -41,32 +48,42 @@ def download_files(value, flag):
     """
     if flag == 1:
         ftp = FTP('ftp.dlptest.com', user='dlpuser@dlptest.com', passwd='e73jzTRTNqCN9PYAAjjn')
-        wav_file_count = 0
         with open('downloaded_from_ftp.pkl', 'rb') as file_obj:
             downloaded_files = pickle.load(file_obj)
-        ftp.cwd('/wav_file/')
-        print ftp.nlst()
+        wav_file_count = len(downloaded_files)
+        # ftp.cwd('/wav_file/')
+        # print ftp.nlst()
         ex = ftp.nlst()
-        with open('list_of_files.pkl', 'rb') as file_obj:
-            old_ex = pickle.load(file_obj)
-        for each_file in ex:
-            if each_file not in old_ex:
-                if each_file[-3:] == 'wav':
-                    wav_file_count = wav_file_count + 1
-                    print wav_file_count
-                    with open(each_file, 'wb') as file_obj:
-                        ftp.retrbinary('RETR '+ each_file, file_obj.write)
-                    downloaded_files.append(each_file)
-                    ex.remove(each_file)
-                    print 'FTP DIr :', ftp.pwd()
-                    ftp.delete(each_file)
+        EX = []
+        # ex = [wav_file for wav_file in ex if wav_file[-3:]=='wav' or 'WAV']
+        for wav in ex:
+            if len(wav) > 3 and ((wav[-3:] == 'wav') or (wav[-3:] == 'WAV')):
+                if ftp.size(wav)/1000 > 870:
+                    EX.append(wav)
+        # with open('list_of_files.pkl', 'rb') as file_obj:
+        #     old_ex = pickle.load(file_obj)
+        for each_file in EX:
+            if each_file not in downloaded_files:
+                if each_file[-3:] == 'wav' or 'WAV':
+                    try:
+                        wav_file_count = wav_file_count + 1
+                        print "Download count:", wav_file_count
+                        with open(each_file, 'wb') as file_obj:
+                            ftp.retrbinary('RETR '+ each_file, file_obj.write)
+                        downloaded_files.append(each_file)
+                        # ex.remove(each_file)
+                        # print 'FTP DIr :', ftp.pwd()
+                        # ftp.delete(each_file)
+                    except:
+                        "print File Error"
                 else:
                     pass
             else:
                 pass
         with open('downloaded_from_ftp.pkl', 'wb') as file_obj:
             pickle.dump(downloaded_files, file_obj)
-        with open('list_of_files.pkl', 'w') as file_obj:
-            pickle.dump(ex, file_obj)
-        ftp.quit()
+        with open('file_count.pkl', 'w') as file_obj:
+            pickle.dump(len(downloaded_files), file_obj)
+        ftp.close()
         return wav_file_count
+
