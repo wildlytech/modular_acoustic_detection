5c1437be72b40e688dcbaabd3fe8381284bf1b2d
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-01-16 12:14:05 +0530

    added condition to wait till file upload is complete before downloading

--- Dash_integration/dash_integrate.py
+++ Dash_integration/dash_integrate.py
@@ -1,15 +1,17 @@
 """
 Creating UI using Dash Library
 """
+import os
 import pickle
 import base64
 import datetime
 import dash
+import glob
 import dash.dependencies
 from dash.dependencies import Input, Output,Event, State
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly
+# import plotly
 import pandas as pd
 import predict_on_wav_file
 import ftp_test
@@ -20,13 +22,23 @@ COLORS = {
     'text': '#7FDBFF'
 }
 
-# Initialize the files and other necessary parameters
-with open('file_count.pkl', 'wb') as f:
-    pickle.dump(0, f)
-with open('downloaded_from_ftp.pkl', 'wb') as f:
-    pickle.dump([], f)
-with open('list_of_files.pkl', 'wb') as f:
-    pickle.dump(['.', '..'], f)
+def check_pre_requiste_files():
+
+    """
+    check if wav files in FTP server are already
+    downloaded locally
+    """
+    files_1 = glob.glob("*.WAV")
+    files_2 = glob.glob("*.wav")
+    files = files_1 + files_2
+    with open('file_count.pkl', 'wb') as file_obj:
+        pickle.dump(len(files), file_obj)
+    with open('downloaded_from_ftp.pkl', 'wb') as file_obj:
+        pickle.dump(files, file_obj)
+    if not os.path.exists('uploaded_files/'):
+        os.makedirs('uploaded_files/')
+    # with open('list_of_files.pkl', 'wb') as f:
+    #   pickle.dump([], f)
 
 
 ################################################################################
@@ -45,8 +57,8 @@ app.layout = html.Div([
                      ])
 
 INDEX_PAGE = html.Div(style={'color': 'green', 'fontSize': 20}, children=[
-    html.H1('Wildly Listen', style={'color': 'green', 'fontSize': 50, 'textAlign':'center', 'text-decoration': 'underline'}),
-    html.H2('A Wildlife Conservation Technology solutions'),
+    html.H1('Wildly Listen', style={'color': 'green', 'fontSize': 50, 'textAlign':'center', 'text-decoration': 'underline' }),
+    html.H2('Acoustic Monitoring and Audio Analysis',style={'color': 'green', 'fontSize': 40, 'textAlign':'center'}),
     html.Img(src='data:image/png;base64,{}'.format(ENCODED_IMAGE), style={'width': '100%',
                                                                           'height':'450px'}),
     html.Br(),
@@ -62,13 +74,12 @@ INDEX_PAGE = html.Div(style={'color': 'green', 'fontSize': 20}, children=[
 
 PAGE_1_LAYOUT = html.Div(id='out-upload-data', children=[
     html.H1('Upload audio Files', style={'color': 'green', 'fontSize': 50, 'textAlign': 'center'}),
-    html.Button('Play sound', id='button_1'),
     html.Div(id='display-play_1'),
     dcc.Upload(
         id='upload-data',
         children=
         html.Div(['Drag and Drop or',
-                  html.A(' Select Files', style={'color': 'green', 'fontSize': 20})]),
+                  html.A(' Select File')], style={'color': 'green', 'fontSize': 20, 'textAlign':'center'}),
         style={
             'width': '100%',
             'height': '60px',
@@ -92,14 +103,30 @@ PAGE_1_LAYOUT = html.Div(id='out-upload-data', children=[
     dcc.Link('FTP status', href='/page-3', style={'marginBottom':20, 'marginTop':45, 'color': 'green', 'fontSize':20})
 ])
 
+
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open('uploaded_files_from_dash/'+name, "wb") as fp:
+        fp.write(base64.b64decode(data))
+
+
+
+
 def parse_contents(contents, filename, date):
     """
     Read the file contents
     """
-    content_type, content_string = contents.split(',')
-
+    # content_type, content_string = contents.split(',')
     if filename[-3:] == 'wav' or 'WAV':
-        predictions_prob, predictions = predict_on_wav_file.main(filename)
+        # create a director to store the uploaded files
+        if not os.path.exists("uploaded_files_from_dash/"):
+            os.makedirs("uploaded_files_from_dash/")
+        # write the uploaded file into the directory created
+        save_file(filename, contents)
+        ENCODED_IMAGE = 'uploaded_files_from_dash/'+ filename
+        ENCODED_IMAGE = base64.b64encode(open(ENCODED_IMAGE, 'rb').read())
+        predictions_prob, predictions = predict_on_wav_file.main('uploaded_files_from_dash/'+filename)
         predictions_prob = [float(i) for i in predictions_prob]
         if predictions[0] == 1:
             output_sound = 'Motor sound'
@@ -116,6 +143,7 @@ def parse_contents(contents, filename, date):
         else:
             output_sound = 'None of the above'
         return  html.Div(style={'color': 'green', 'fontSize':14}, children=[
+            html.Audio(id='myaudio', src='data:audio/WAV;base64,{}'.format(ENCODED_IMAGE), controls=True, title=True),  
             html.H4('predictions rounded will be: '+ str(predictions)),
             html.H4('Predictions seems to be '+ output_sound, style={'color': 'green', 'fontSize': 30, 'textAlign':'center', 'text-decoration':'underline'}),
             dcc.Graph(id='example',
@@ -156,7 +184,7 @@ def parse_contents(contents, filename, date):
                                   },
                       style={'marginBottom': 20, 'marginTop': 45, 'color':'black'}),
             html.P('Uploaded File : '+ filename, style={'color': 'black', 'fontSize': 12}),
-            html.P('Last Modified : '+ str(datetime.datetime.fromtimestamp(date)), style={'color': 'black', 'fontSize': 12})
+            # html.P('Last Modified : '+ str(datetime.datetime.fromtimestamp(date)), style={'color': 'black', 'fontSize': 12})
                         ])
     else:
         return html.Div([
@@ -181,51 +209,7 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
-# callback function for play button disabling and enabling
-@app.callback(Output('button_1', 'disabled'),
-              [Input(component_id='upload-data', component_property='content')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def button_enable_disable(content, filename, date):
-    """
-    enabling and disabling the button
-    """
-    if filename[0]:
-        return False
-    else:
-        return True
 
-#callback function to play the audio
-@app.callback(Output('display-play_1', 'children'),
-              [Input('button_1', 'n_clicks'), Input(component_id='upload-data', component_property='content')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def play_sound(n_clicks, contents, filename,date):
-    """
-    audio playable option
-    """
-    print n_clicks
-    print filename[0]
-    if (filename[0] != None) and  (n_clicks > 0):
-        from pydub import AudioSegment
-        from pydub.playback import play
-
-        song = AudioSegment.from_wav(filename[0])
-        play(song)
-        return
-
-#call back function to check and update the clicks
-@app.callback(Output('button_1', 'n_clicks'),
-              [Input(component_id='upload-data', component_property='filename')],
-              [State('upload-data', 'filename'),
-               State('upload-data', 'last_modified')])
-def play_sound(n_clicks, content, filename, date):
-    """
-    updating the clicks
-    """
-    if filename[0]:
-        n_clicks = 0
-        return n_clicks
 
 
 ################################################################################
@@ -234,7 +218,7 @@ def play_sound(n_clicks, content, filename, date):
 
 
 PAGE_2_LAYOUT = html.Div(id='Wildly listen', children=[
-    html.H1('Its a wildlife Conservation Tech company', style={'textAlign':'center', 'color':'green'}),
+    html.H1('Acoustic Monitoring and Audio Analysis', style={'textAlign':'center', 'color':'green'}),
     dcc.Dropdown(
         id='my-dropdown',
         options=[
@@ -282,10 +266,11 @@ def update_values(input_data):
 
 PAGE_3_LAYOUT = html.Div(style={'color': 'green', 'fontSize': 20}, children=[
     html.H1('FTP status', style={'color': 'green', 'fontSize': 50, 'textAlign':'center', 'text-decoration':'underline'}),
+    html.P('Note : Downloads if any wav files are present in the remote FTP server ', style={'color': 'green', 'fontSize': 12}),
     html.Div(id='page-3-content-2'),
     dcc.Interval(
         id='interval-component',
-        interval=1*20000, # in milliseconds
+        interval=1*30000, # in milliseconds
         n_intervals=0
     ),
     dcc.Link('Input audio file', href='/page-1', style={'marginBottom': 20, 'marginTop': 45, 'color': 'green'}),
@@ -302,35 +287,36 @@ def check_for_data(n_intervals):
     check for .wav files in ftp server
     for every specified seconds
     """
-    wav_files, wav_files_count, flag = ftp_test.ftp_data()
+    check_pre_requiste_files()
+    wav_files, wav_files_count = ftp_test.ftp_data()
+    print wav_files
+    print "scraped count:", wav_files_count
     with open('file_count.pkl', 'rb') as f:
         old_count = pickle.load(f)
     with open('downloaded_from_ftp.pkl', 'rb') as f:
         downloaded_files = pickle.load(f)
     if wav_files_count >= 1:
-        downloaded_files = downloaded_files + wav_files
-        with open('downloaded_from_ftp.pkl', 'w') as f:
-            pickle.dump(downloaded_files, f)
+        download_count = ftp_test.download_files(1, wav_files_count)
     else:
         pass
     print 'Wav file count :', wav_files_count
-    old_count = old_count + wav_files_count
-    print 'New count :', old_count
-    with open('file_count.pkl', 'wb') as f:
-        pickle.dump(old_count, f)
-    if wav_files_count == 1:
+    if wav_files_count >= 1:
         return html.Div(children=[
-            html.H2('New File uploaded..!!'),
-            html.P('Downloading to local drive..', style={'marginBottom':20, 'marginTop':45, 'color':'black'})
+            html.H2('New File/s uploaded..!!')
+            # html.P('Downloading to local drive..', style={'marginBottom':20, 'marginTop':45, 'color':'black'})
                                  ])
     elif  wav_files_count == 0:
+      with open('file_count.pkl', 'rb') as file_obj:
+        download_count = pickle.load(file_obj)
         return html.Div(children=[
-            html.H4('Total number of wav files downloaded :' + str(old_count)),
+            html.H4('Total number of wav files in FTP server :' + str(download_count)),
             dcc.Link('Run analysis', href='/page-4', style={'marginBottom':20, 'marginTop':45, 'color':'green'})
                                   ])
     else:
+      with open('file_count.pkl', 'rb') as file_obj:
+        download_count = pickle.load(file_obj)
         return  html.Div(children=[
-            html.H4('Total number of wav files downloaded :' + str(old_count)),
+            html.H4('Total number of wav files in FTP server :' + str(download_count)),
             dcc.Link('Run analysis', href='/page-4', style={'marginBottom': 20, 'marginTop': 45, 'color': 'green'})
                                   ])    
 
@@ -345,7 +331,7 @@ PAGE_4_LAYOUT = html.Div(style={'color': 'green', 'fontSize': 20}, children=[
     html.Div(id='display-play'),
     dcc.Interval(
         id='interval-component',
-        interval=1*15000, # in milliseconds
+        interval=1*20000, # in milliseconds
         n_intervals=0
     ),
     dcc.Dropdown(
@@ -375,11 +361,9 @@ def play_sound(n_clicks, dropdown_value):
     """
     print n_clicks
     if (dropdown_value != None) and (n_clicks > 0):
-        from pydub import AudioSegment
-        from pydub.playback import play
-        song = AudioSegment.from_wav(dropdown_value)
-        play(song)
-        return
+        ENCODED_IMAGE = dropdown_value   
+        ENCODED_IMAGE = base64.b64encode(open(ENCODED_IMAGE, 'rb').read())
+        return  html.Audio(id='myaudio', src='data:audio/WAV;base64,{}'.format(ENCODED_IMAGE), controls=True,  title=True)
 
 # callback for updating the clicks
 @app.callback(Output('button', 'n_clicks'),
@@ -417,7 +401,7 @@ def predict_on_downloaded_file(input_name):
     actual predictions takes place here
     """
     print input_name
-    if input_name[-3:] == 'wav':
+    if input_name[-3:] == 'wav' or 'WAV':
         predictions_prob, predictions = predict_on_wav_file.main(input_name)
         predictions_prob = [float(i) for i in predictions_prob]
         if predictions[0] == 1:
@@ -439,7 +423,7 @@ def predict_on_downloaded_file(input_name):
                 html.H4('Predictions seems to be '+ output_sound,style={'color': 'green', 'fontSize': 30,'textAlign': 'center','text-decoration':'underline'}),
                 dcc.Graph(id='example',
                           figure={
-                                 'data':[{'x':['Motor', 'Motor_2', 'Human', 'Nature', 'Domestic', 'Tools'], 'y':[i*100 for i in predictions_prob], 'marker':{
+                                 'data':[{'x':['Motor', 'Explosion', 'Human', 'Nature', 'Domestic', 'Tools'], 'y':[i*100 for i in predictions_prob], 'marker':{
                                      'color':['rgba(26, 118, 255,0.8)', 'rgba(222,45,38,0.8)',
                                               'rgba(204,204,204,1)', 'rgba(0,150,0,0.8)',
                                               'rgba(204,204,204,1)', 'rgba(55, 128, 191, 0.7)']}, 'type':'bar'}],
@@ -492,4 +476,5 @@ def display_page(pathname):
 
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=True)
+
 
