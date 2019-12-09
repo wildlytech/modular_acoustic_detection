fa73a868397913f50b8ebb678872057ba03bc610
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-12-09 14:07:50 +0530

    to download all the google youtube audioset combining ambient and impact sounds

---
+++ get_data/download_all_sounds.py
@@ -0,0 +1,14 @@
+"""
+Downloads all the Impact and Ambient sounds
+"""
+import download_soi
+from download_soi import download_data, AMBIENT_SOUNDS, IMPACT_SOUNDS
+
+########################################################################
+			# Main Function
+########################################################################
+if __name__ == "__main__":
+
+    TARGET_SOUNDS = AMBIENT_SOUNDS + IMPACT_SOUNDS
+    TARGET_PATH = 'sounds/ambient_impact/'
+    download_data(TARGET_SOUNDS, TARGET_PATH)
