84c76f434aa0a6b536142a5fd9535136181cce3a
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-12-13 15:32:00 +0530

    get the prediction for unlabelled dataframe using saved goertzel model

--- goertzel_filter/predictions_goertzel_model/predictions_using_goertzel_model.py
+++ goertzel_filter/predictions_goertzel_model/predictions_using_goertzel_model.py
@@ -1,32 +1,46 @@
 """
 predictions are done on the audiomoth files
 """
+import sys
+import pickle
 import argparse
+import numpy as np
 import keras
-from keras.models import Sequential, Model
+from keras.models import Model
 from keras.layers import Input
-import predictions_on_weights
-from keras.layers import Dense, Conv1D, Conv2D, MaxPooling1D, Flatten, AveragePooling1D, TimeDistributed, MaxPooling2D
-import audiomoth_function_for_goertzel_model as aud_goertzel
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, TimeDistributed, MaxPooling2D
 
 
-# Define the constants
+
+#################################################################################
+          # Description and Help
+#################################################################################
 DESCRIPTION = "Compares the prediction my goertzel model and annotated labels"
-HELP_AUDIO = "Path for audio files ( .WAV )"
-HELP_GOERTZEL = "Path for Goertzel freq components ( .pkl )"
+HELP_AUDIO = "Path for Dataframe with features( .pkl )"
+HELP_GOERTZEL = "Path to write predictions in csv( .csv )"
 
 
-#parse the input arguments given from command line
+
+#################################################################################
+          # parse the input arguments given from command line
+#################################################################################
 PARSER = argparse.ArgumentParser(description=DESCRIPTION)
-PARSER.add_argument('-audio_files_path',
-                    '--audio_files_path', action='store',
+PARSER.add_argument('-path_dataframe_with_features',
+                    '--path_dataframe_with_features', action='store',
                     help=HELP_AUDIO)
-PARSER.add_argument('-path_for_goertzel_components',
-                    '--path_for_goertzel_components', action='store',
+PARSER.add_argument('-path_for_saved_weights',
+                    '--path_for_saved_weights', action='store',
+                    help='Weights file (.h5)')
+PARSER.add_argument("-path_to_write_prediction_csv", action="store",
                     help=HELP_GOERTZEL)
 RESULT = PARSER.parse_args()
 
-#Define the goertzel model same as model from which the weights are saved
+
+
+
+#################################################################################
+    # Define the goertzel model same as model from which the weights are saved
+#################################################################################
 INPUTS = Input(shape=(10, 8000, 4))
 CONV_1 = TimeDistributed(Conv1D(100,
                                 kernel_size=200,
@@ -47,20 +61,67 @@ PREDICTIONS = Flatten()(MAX_POOL_2)
 MODEL = Model(inputs=[INPUTS], outputs=[PREDICTIONS])
 print MODEL.summary()
 
-# Load the saved weights and predict on the audiomoth  recordings
-MODEL.load_weights('../Goertzel_model_8k_weights_time.h5')
 
-#call the audiomoth function to predict on the frequency components of audio moth WAV files
-TEST_VALUES, DATAFRAME = aud_goertzel.dataframe_with_frequency_components(RESULT.audio_files_path,
-                                                                          RESULT.path_for_goertzel_components)
-PREDICTIONS = MODEL.predict(TEST_VALUES).ravel()
-DATAFRAME['predictions_prob'] = PREDICTIONS
-DATAFRAME['predictions'] = PREDICTIONS.ravel().round()
 
-#save it in a CSV file
-DATAFRAME[['wav_file',
-           'Label_1',
-           'Label_2',
-           'Label_3',
-           'predictions_prob',
-           'predictions']].to_csv('predictions_by_goertzel_model.csv')
+#################################################################################
+      # Load the saved weights and predict on the audiomoth  recordings
+#################################################################################
+
+if RESULT.path_for_saved_weights:
+    MODEL.load_weights(RESULT.path_for_saved_weights)
+else:
+    print "No weights File given."
+    sys.exit(1)
+
+#################################################################################
+      # Read the dataframe with columns features and labels name
+#################################################################################
+if RESULT.path_dataframe_with_features:
+    with open(RESULT.path_dataframe_with_features, "rb") as file_obj:
+        DF_TEST = pickle.load(file_obj)
+else:
+    print "Dataframe not Found."
+    sys.exit(1)
+
+
+#################################################################################
+            # Read all the test data first
+#################################################################################
+CLF1_TEST = []
+print 'Reading Test files ..!'
+
+for each_emb, each_wav in zip(DF_TEST['features'].tolist(), DF_TEST["wav_file"].tolist()):
+    # Read all the files that are splitted as test in the path directory specified
+    try:
+        CLF1_TEST.append(np.dstack((each_emb[0].reshape((10, 8000)),
+                                    each_emb[1].reshape((10, 8000)),
+                                    each_emb[2].reshape((10, 8000)),
+                                    each_emb[3].reshape((10, 8000)))))
+
+    #except any error then remove that file manually and then run the process again
+    except OSError:
+        print 'Test Pickling Error: ', each_wav
+
+
+#################################################################################
+        # Preprocess the data as per the input of the model
+#################################################################################
+CLF1_TEST = np.array(CLF1_TEST).reshape((-1, 10, 8000, 4))
+CLF1_TEST = CLF1_TEST / np.linalg.norm(CLF1_TEST)
+
+
+#################################################################################
+        # Run the predictions on the data
+#################################################################################
+PREDICTIONS = MODEL.predict(CLF1_TEST).ravel()
+DF_TEST['predictions_prob'] = PREDICTIONS
+DF_TEST['predictions'] = PREDICTIONS.ravel().round()
+
+
+#################################################################################
+          # save it in a CSV file
+#################################################################################
+if RESULT.path_to_write_prediction_csv:
+    DF_TEST.drop(["features"], axis=1).to_csv(RESULT.path_to_write_prediction_csv)
+else:
+    print "Predictions are not saved. Give appropiate argument to save the predictions"
