14bce3b57c7d2e76d33cc6d55aab9a8a34d0a0ee
ShivarajMeti <shivaraj.meti002@gmail.com>
2019-12-16 12:01:34 +0530

    changed as per final model architecture

--- goertzel_filter/predictions_goertzel_model/predictions_using_goertzel_model.py
+++ goertzel_filter/predictions_goertzel_model/predictions_using_goertzel_model.py
@@ -42,22 +42,20 @@ RESULT = PARSER.parse_args()
     # Define the goertzel model same as model from which the weights are saved
 #################################################################################
 INPUTS = Input(shape=(10, 8000, 4))
-CONV_1 = TimeDistributed(Conv1D(100,
-                                kernel_size=200,
-                                strides=100,
+MAXPOOL_1 = TimeDistributed(MaxPooling1D(80))(INPUTS)
+CONV_1 = TimeDistributed(Conv1D(20,
+                                kernel_size=100,
+                                strides=50,
                                 activation='relu',
-                                padding='same'),
-                         input_shape=(10, 8000, 4))(INPUTS)
-CONV_2 = TimeDistributed(Conv1D(100,
-                                kernel_size=4,
-                                activation='relu',
-                                padding='same'))(CONV_1)
-MAX_POOL = TimeDistributed(MaxPooling1D(80))(CONV_2)
-DENSE_1 = TimeDistributed(Dense(60, activation='relu'))(MAX_POOL)
+                                padding='same'))(MAXPOOL_1)
+MAXPOOL_2 = TimeDistributed(MaxPooling1D(2))(CONV_1)
+DENSE_1 = TimeDistributed(Dense(50, activation='relu'))(MAXPOOL_2)
 DENSE_2 = TimeDistributed(Dense(50, activation='relu'))(DENSE_1)
-DENSE_3 = TimeDistributed(Dense(1, activation='sigmoid'))(DENSE_2)
-MAX_POOL_2 = MaxPooling2D((10, 1))(DENSE_3)
-PREDICTIONS = Flatten()(MAX_POOL_2)
+DENSE_3 = TimeDistributed(Dense(50, activation='relu'))(DENSE_2)
+DENSE_4 = TimeDistributed(Dense(50, activation='relu'))(DENSE_3)
+DENSE_5 = TimeDistributed(Dense(1, activation='sigmoid'))(DENSE_4)
+MAX_POOL_3 = MaxPooling2D((10, 1))(DENSE_5)
+PREDICTIONS = Flatten()(MAX_POOL_3)
 MODEL = Model(inputs=[INPUTS], outputs=[PREDICTIONS])
 print MODEL.summary()
 
