40145815b9d29ea9ccf6d2f3b5fcd297c05144c2
aryaman.sriram <aryaman.sriram@gmail.com>
2020-11-21 00:18:01 +0530

    Made filepath arg optional

--- display_spec.py
+++ display_spec.py
@@ -17,7 +17,7 @@ def get_spectrogram(filepath):
     S = librosa.feature.melspectrogram(y=sig, sr=fs)
     return S
 
-def plot_spec(S,filepath):
+def plot_spec(S,filepath=None):
     """
     Create plot and save the same in the given output filepath
 
@@ -30,16 +30,23 @@ def plot_spec(S,filepath):
     ax = plt.subplot(111)
     fig = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), x_axis="time", y_axis="log", ax=ax)
     plt.colorbar(fig, ax=ax)
-    plt.savefig(filepath, bbox_inches='tight', pad_inches=0)
-    plt.show()
+    if filepath!=None:
+        plt.savefig(filepath, bbox_inches='tight', pad_inches=0)
+    else:
+        plt.show()
 
 if __name__=="__main__":
     DESCRIPTION = "SPECTROGRAM DISPLAY MODULE"
     PARSER = argparse.ArgumentParser(description=DESCRIPTION)
     REQUIRED_ARGUMENTS =  PARSER.add_argument_group("required arguments")
+    OPTIONAL_ARGUMENTS = PARSER.add_argument_group("optional arguments")
     REQUIRED_ARGUMENTS.add_argument("-input_file_path",action="store",help="Input filepath to sound file",required=True)
-    REQUIRED_ARGUMENTS.add_argument("-output_file_path",action="store",help="Output file path of location where plot is to be saved",required=True)
+    OPTIONAL_ARGUMENTS.add_argument("-output_file_path",action="store",help="Output file path of location where plot is to be saved")
     RESULT = PARSER.parse_args()
 
     S = get_spectrogram(RESULT.input_file_path)
-    plot_spec(S,RESULT.output_file_path)
+    if result.output_file_path:
+        plot_spec(S,RESULT.output_file_path)
+    else:
+        plot_spec(S)
+
