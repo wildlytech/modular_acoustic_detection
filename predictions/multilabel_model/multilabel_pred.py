ffa5f3d141b907f068f1dcaae63549398b7a6af3
aryaman.sriram <aryaman.sriram@gmail.com>
2020-12-20 10:53:19 +0530

    Updated pred file

--- predictions/multilabel_model/multilabel_pred.py
+++ predictions/multilabel_model/multilabel_pred.py
@@ -43,9 +43,9 @@ def import_predict_configuration_json(predictions_cfg_json):
 
                 # If single file or null, then convert to list
                 if ontologyExtFiles is None:
-                  ontologyExtFiles = []
+                    ontologyExtFiles = []
                 elif type(ontologyExtFiles) != list:
-                  ontologyExtFiles = [ontologyExtFiles]
+                    ontologyExtFiles = [ontologyExtFiles]
 
 
 
