4a491b3ae14670e253174ef69c6a9540a192985b
aryaman.sriram <aryaman.sriram@gmail.com>
2021-02-10 11:03:48 +0530

    Removed bs4 and added youtube-search support

--- get_data/youtube_scrape.py
+++ get_data/youtube_scrape.py
@@ -1,16 +1,13 @@
-from bs4 import BeautifulSoup as bs
-import requests
+from youtubesearchpython import VideosSearch
 import os
 from subprocess import check_call, CalledProcessError
-
 
 ###########################################################################
 # Change as per the need
 ###########################################################################
 SEARCH_KEYWORD = "dog barking"
 PATH_TO_WRITE_AUDIO = "youtube_scraped_audio/"
-NUMBER_AUDIOCLIPS_LIMIT = 10
-
+NUMBER_AUDIOCLIPS_LIMIT = 5
 
 ###########################################################################
 # create the directory path if not present
@@ -24,28 +21,15 @@ if not os.path.exists(PATH_TO_WRITE_AUDIO + "_".join(SEARCH_KEYWORD.split(" ")))
 else:
     pass
 
-
 ###########################################################################
 # Settings for scraping the youtube webpage with selected query keyword
 ###########################################################################
-QUERY_CALL = SEARCH_KEYWORD
-BASE_QUERY = "https://www.youtube.com/results?search_query="
-QSTRING = "+".join(QUERY_CALL.split(" "))
-REQUESTS_CALL = requests.get(BASE_QUERY + QSTRING)
-PAGE = REQUESTS_CALL.text
-SOUP = bs(PAGE, 'html.parser')
-VIDEOS = SOUP.findAll('a', attrs={'class': 'yt-uix-tile-link'})
-
-
-###########################################################################
-# Create a list of query's with videoID's
-###########################################################################
+vid_search = VideosSearch(SEARCH_KEYWORD, limit=10)
 VIDEO_LIST = []
-for v in VIDEOS:
-    tmp = 'https://www.youtube.com' + v['href']
-    VIDEO_LIST.append(tmp)
+for result_dict in vid_search.result()["result"]:
+    VIDEO_LIST.append(result_dict["link"])
 
-
+print("Links: ", VIDEO_LIST)
 ###########################################################################
 # Download audio file onto the target directory
 ###########################################################################
@@ -53,7 +37,7 @@ COUNT = 0
 for item in VIDEO_LIST[:NUMBER_AUDIOCLIPS_LIMIT]:
     COUNT += 1
     path = PATH_TO_WRITE_AUDIO + "_".join(SEARCH_KEYWORD.split(" ")) + "/" + \
-        SEARCH_KEYWORD + "_" + item.split("=")[-1] + "_" + str(COUNT)
+           SEARCH_KEYWORD + "_" + item.split("=")[-1] + "_" + str(COUNT)
     if not os.path.exists(path):
         try:
             check_call(['youtube-dl', item,
