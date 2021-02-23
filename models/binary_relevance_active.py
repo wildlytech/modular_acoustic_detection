fdf3e80e84b53d5759c155a800cb48af55b17581
aryaman.sriram <aryaman.sriram@gmail.com>
2021-02-23 12:57:17 +0530

    Fixed cpu integration

--- models/binary_relevance_active.py
+++ models/binary_relevance_active.py
@@ -34,8 +34,8 @@ DESCRIPTION = "Reads the configuration file to train a particular \
 
 # GPU Memory Growth
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if len(physical_devices) > 0:
+    config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 #############################################################################
 # Parse the input arguments given from command line
 #############################################################################
