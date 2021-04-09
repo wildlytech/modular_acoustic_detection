97e7ff423b6debf0cee14308acb6137e8ef58bfe
aryaman.sriram <aryaman.sriram@gmail.com>
2021-04-09 11:30:42 +0530

    Added finetuning script

---
+++ externals/models.py
@@ -0,0 +1,349 @@
+'''
+The MIT License
+
+Copyright (c) 2018-2020 Qiuqiang Kong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+from torchlibrosa import Spectrogram,LogmelFilterBank,SpecAugmentation
+
+def init_layer(layer):
+    nn.init.xavier_uniform_(layer.weight)
+
+    if hasattr(layer, "bias"):
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.)
+
+
+def init_bn(bn):
+    bn.bias.data.fill_(0.)
+    bn.weight.data.fill_(1.0)
+
+
+def interpolate(x: torch.Tensor, ratio: int):
+    """Interpolate data in time domain. This is used to compensate the
+    resolution reduction in downsampling of a CNN.
+
+    Args:
+      x: (batch_size, time_steps, classes_num)
+      ratio: int, ratio to interpolate
+    Returns:
+      upsampled: (batch_size, time_steps * ratio, classes_num)
+    """
+    (batch_size, time_steps, classes_num) = x.shape
+    upsampled = x[:, :, None, :].repeat(1, 1, ratio, 1)
+    upsampled = upsampled.reshape(batch_size, time_steps * ratio, classes_num)
+    return upsampled
+
+
+def pad_framewise_output(framewise_output: torch.Tensor, frames_num: int):
+    """Pad framewise_output to the same length as input frames. The pad value
+    is the same as the value of the last frame.
+    Args:
+      framewise_output: (batch_size, frames_num, classes_num)
+      frames_num: int, number of frames to pad
+    Outputs:
+      output: (batch_size, frames_num, classes_num)
+    """
+    pad = framewise_output[:, -1:, :].repeat(
+        1, frames_num - framewise_output.shape[1], 1)
+    """tensor for padding"""
+
+    output = torch.cat((framewise_output, pad), dim=1)
+    """(batch_size, frames_num, classes_num)"""
+
+    return output
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+            bias=False)
+
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.init_weight()
+
+    def init_weight(self):
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+
+    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
+
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        x = F.relu_(self.bn2(self.conv2(x)))
+        if pool_type == 'max':
+            x = F.max_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg':
+            x = F.avg_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg+max':
+            x1 = F.avg_pool2d(x, kernel_size=pool_size)
+            x2 = F.max_pool2d(x, kernel_size=pool_size)
+            x = x1 + x2
+        else:
+            raise Exception('Incorrect argument!')
+
+        return x
+
+
+class AttBlock(nn.Module):
+    def __init__(self,
+                 in_features: int,
+                 out_features: int,
+                 activation="linear",
+                 temperature=1.0):
+        super().__init__()
+
+        self.activation = activation
+        self.temperature = temperature
+        self.att = nn.Conv1d(
+            in_channels=in_features,
+            out_channels=out_features,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=True)
+        self.cla = nn.Conv1d(
+            in_channels=in_features,
+            out_channels=out_features,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=True)
+
+        self.bn_att = nn.BatchNorm1d(out_features)
+        self.init_weights()
+
+    def init_weights(self):
+        init_layer(self.att)
+        init_layer(self.cla)
+        init_bn(self.bn_att)
+
+    def forward(self, x):
+        # x: (n_samples, n_in, n_time)
+        norm_att = torch.softmax(torch.tanh(self.att(x)), dim=-1)
+        cla = self.nonlinear_transform(self.cla(x))
+        x = torch.sum(norm_att * cla, dim=2)
+        return x, norm_att, cla
+
+    def nonlinear_transform(self, x):
+        if self.activation == 'linear':
+            return x
+        elif self.activation == 'sigmoid':
+            return torch.sigmoid(x)
+
+
+class CustomPanns(nn.Module):
+    def __init__(self, sample_rate: int, window_size: int, hop_size: int,
+                 mel_bins: int, fmin: int, fmax: int,act_classes: int, classes_num: int, apply_aug: bool,wts_path: str, top_db=None):
+        super().__init__()
+
+        self.bn0 = nn.BatchNorm2d(mel_bins)
+
+        self.fc1 = nn.Linear(1024, 1024, bias=True)
+        self.attn = AttBlock(1024, act_classes, activation='sigmoid')
+        self.backbone = PANNsDense121Att(sample_rate, window_size, hop_size,
+                                         mel_bins, fmin, fmax, classes_num, apply_aug,top_db)
+        checkpoint = torch.load(wts_path)
+
+        self.backbone.load_state_dict(checkpoint["model"])
+        for param in self.backbone.parameters():
+            param.requires_grad=False
+
+
+    def forward(self,input_data):
+        input_x, mixup_lambda = input_data
+        """
+        Input: (batch_size, data_length)"""
+        b, c, s = input_x.shape
+
+
+
+        #frame_shape = framewise_output.shape
+        #clip_shape = clipwise_output.shape
+        #combined = torch.cat([clipwise_output,clip_bkbone],dim=1)
+        #combined = combined/torch.sum(combined)
+        '''output_dict = {
+            'framewise_output': framewise_output.reshape(b, c, frame_shape[1], frame_shape[2]),
+            'clipwise_output': clipwise_output.reshape(b, c, clip_shape[1]),
+            "combined_output": combined.reshape(b,c,combined.shape[1]),
+            "backbone_output": clip_bkbone.reshape(b,c,clip_bkbone.shape[1])
+        }'''
+        features,output_dict = self.backbone.forward(input_data)
+
+
+        (clipwise_output, norm_att, segmentwise_output) = self.attn(features)
+        #segmentwise_output = segmentwise_output.transpose(1, 2)
+
+        #framewise_output = interpolate(segmentwise_output,
+        #                              self.backbone.interpolate_ratio)
+        #framewise_output = pad_framewise_output(framewise_output, frames_num)
+        #print(output_dict["clipwise_output"].shape)
+        #print(clipwise_output.shape)
+        combined = torch.cat([output_dict["clipwise_output"],clipwise_output.unsqueeze(1)],axis=2)
+        #combined = torch.softmax(combined,dim=2)
+        output_dict["combined_output"] = combined.reshape(b,c,combined.shape[2])
+        return output_dict
+
+
+
+
+
+
+class PANNsDense121Att(nn.Module):
+    def __init__(self, sample_rate: int, window_size: int, hop_size: int,
+                 mel_bins: int, fmin: int, fmax: int, classes_num: int, apply_aug: bool, top_db=None):
+        super().__init__()
+
+        window = 'hann'
+        center = True
+        pad_mode = 'reflect'
+        ref = 1.0
+        amin = 1e-10
+        self.interpolate_ratio = 32  # Downsampled ratio
+        self.apply_aug = apply_aug
+
+        # Spectrogram extractor
+        self.spectrogram_extractor = Spectrogram(
+            n_fft=window_size,
+            hop_length=hop_size,
+            win_length=window_size,
+            window=window,
+            center=center,
+            pad_mode=pad_mode,
+            freeze_parameters=True)
+
+        # Logmel feature extractor
+        self.logmel_extractor = LogmelFilterBank(
+            sr=sample_rate,
+            n_fft=window_size,
+            n_mels=mel_bins,
+            fmin=fmin,
+            fmax=fmax,
+            ref=ref,
+            amin=amin,
+            top_db=top_db,
+            freeze_parameters=True)
+
+        # Spec augmenter
+        self.spec_augmenter = SpecAugmentation(
+            time_drop_width=64,
+            time_stripes_num=2,
+            freq_drop_width=8,
+            freq_stripes_num=2)
+
+        self.bn0 = nn.BatchNorm2d(mel_bins)
+
+        self.fc1 = nn.Linear(1024, 1024, bias=True)
+        self.att_block = AttBlock(1024, classes_num, activation='sigmoid')
+
+        self.densenet_features = models.densenet121(pretrained=False).features
+
+        self.init_weight()
+
+    def init_weight(self):
+        init_bn(self.bn0)
+        init_layer(self.fc1)
+
+    def cnn_feature_extractor(self, x):
+        x = self.densenet_features(x)
+        return x
+
+    def preprocess(self, input_x, mixup_lambda=None):
+
+        x = self.spectrogram_extractor(input_x)  # (batch_size, 1, time_steps, freq_bins)
+        x = self.logmel_extractor(x)  # (batch_size, 1, time_steps, mel_bins)
+
+        frames_num = x.shape[2]
+
+        x = x.transpose(1, 3)
+        x = self.bn0(x)
+        x = x.transpose(1, 3)
+
+        if self.apply_aug:
+            x = self.spec_augmenter(x)
+
+        return x, frames_num
+
+    def forward(self, input_data):
+        input_x, mixup_lambda = input_data
+        """
+        Input: (batch_size, data_length)"""
+        b, c, s = input_x.shape
+        input_x = input_x.reshape(b * c, s)
+        x, frames_num = self.preprocess(input_x, mixup_lambda=mixup_lambda)
+        if mixup_lambda is not None:
+            b = (b * c) // 2
+            c = 1
+        # Output shape (batch size, channels, time, frequency)
+        x = x.expand(x.shape[0], 3, x.shape[2], x.shape[3])
+        x = self.cnn_feature_extractor(x)
+
+        # Aggregate in frequency axis
+        x = torch.mean(x, dim=3)
+
+        x1 = F.max_pool1d(x, kernel_size=3, stride=1, padding=1)
+        x2 = F.avg_pool1d(x, kernel_size=3, stride=1, padding=1)
+        x = x1 + x2
+
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = x.transpose(1, 2)
+        x = F.relu_(self.fc1(x))
+        x = x.transpose(1, 2)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        (clipwise_output, norm_att, segmentwise_output) = self.att_block(x)
+        segmentwise_output = segmentwise_output.transpose(1, 2)
+
+        # Get framewise output
+        framewise_output = interpolate(segmentwise_output,
+                                       self.interpolate_ratio)
+        framewise_output = pad_framewise_output(framewise_output, frames_num)
+        frame_shape = framewise_output.shape
+        clip_shape = clipwise_output.shape
+        output_dict = {
+            'framewise_output': framewise_output.reshape(b, c, frame_shape[1], frame_shape[2]),
+            'clipwise_output': clipwise_output.reshape(b, c, clip_shape[1]),
+        }
+
+        return x,output_dict
---
+++ models/panns_xeno_finetune.py
@@ -0,0 +1,269 @@
+from externals.models import CustomPanns
+import librosa
+import numpy as np
+import torch
+from torch.utils.data import Dataset,DataLoader,SequentialSampler
+import torch.nn as nn
+from torch.optim import Adam
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from model_configs.panns import train_config
+import tqdm
+
+
+# SEED EVERYTHING
+torch.manual_seed(42)
+np.random.seed(42)
+
+
+
+model_config = config["MODEL_CONFIG"]
+
+model = CustomPanns(
+    **model_config
+)
+
+
+PERIOD = config["PERIOD"]
+SR = config["SR"]
+vote_lim = config["vote_lim"]
+TTA = config["TTA"]
+BATCH_SIZE=config["BATCH_SIZE"]
+BIRD_CODE = config["BIRD_CODE"]
+INV_BIRD_CODE = config["INV_BIRD_CODE"]
+
+
+def df_checker(df_path,SR=32000):
+    df = pd.read_csv(df_path)
+    new_df_dict = {}
+    new_df_dict["filepath"] = []
+    new_df_dict["bird_name"] = []
+    new_df_dict["ebird_code"] = []
+    for ii,filename in enumerate(df["filepath"]):
+        print("Iteration: ",ii+1)
+
+        try:
+
+            audio,_ = librosa.load(filename,sr=SR,mono=True,res_type="kaiser_fast")
+            new_df_dict["filepath"].append(filename)
+            new_df_dict["bird_name"].append(df["bird_name"][ii])
+            new_df_dict["ebird_code"].append(df["ebird_code"][ii])
+
+        except Exception as e:
+            print(e)
+            inp = input("Enter y to break")
+            if inp=="y":
+                break
+            print(new_df_dict)
+
+
+    return pd.DataFrame(new_df_dict)
+#csv_path = "kaggle_1_soln/test_df.csv"
+#df = df_checker(csv_path)
+#df.to_csv("kaggle_1_soln/checked_df.csv")
+class XC_Dataset(Dataset):
+    def __init__(self,csv,enc_dict,num_classes):
+        self.csv = csv
+        self.enc_dict = enc_dict
+        self.num_classes = num_classes
+
+    def __len__(self):
+        return len(self.csv)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        path = self.csv["filepath"][idx]
+
+        label = self.csv["ebird_code"][idx]
+        zs = np.zeros((1, self.num_classes))
+        zs[:,self.enc_dict[label]] = 1
+        clip,_ = librosa.load(path,sr=SR,mono=True,res_type="kaiser_fast")
+        array = clip.astype(np.float32)
+        if len(array)<960000:
+            array = np.pad(array,(0,960000-len(array)),mode='constant',constant_values=0)
+        #print(path)
+        #print(len(array))
+        tensors = torch.from_numpy(array)
+
+        label = torch.Tensor(zs)
+        return tensors,label
+
+data_csv = pd.read_csv("kaggle_1_soln/checked_df_train.csv")
+
+test_data_csv = pd.read_csv("kaggle_1_soln/checked_df_test.csv")
+
+df_train,df_val = train_test_split(data_csv,test_size=0.2,random_state=42)
+df_train.reset_index(inplace=True)
+df_val.reset_index(inplace=True)
+train_dset = XC_Dataset(df_train,BIRD_CODE,268)
+val_dset = XC_Dataset(df_val,BIRD_CODE,268)
+test_dset = XC_Dataset(test_data_csv,BIRD_CODE,268)
+
+train_dataloader = DataLoader(train_dset,batch_size=BATCH_SIZE,shuffle=True,num_workers=4)
+val_dataloader = DataLoader(val_dset,batch_size=BATCH_SIZE,num_workers=4)
+test_dataloader = DataLoader(test_dset,batch_size=BATCH_SIZE,num_workers=4)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model.to(device)
+EPOCHS = config["EPOCHS"]
+criterion = config["LOSS"]
+optimizer = config["OPTIM"]
+optim = optimizer(model.parameters(),lr=config["LR"])
+
+
+master_bar = tqdm.trange(EPOCHS,unit="Epochs")
+
+for epoch in master_bar:
+    #print("Epoch: ",epoch+1," of ",EPOCHS)
+    acc_score = 0.0
+    model.train()
+    pbar_t = tqdm.tqdm(train_dataloader,unit="batch",leave=False)
+    pbar_t.set_description(f"Epoch: {epoch}")
+    for ii,(x,y) in enumerate(pbar_t):
+        optim.zero_grad()
+        image = x.unsqueeze(1)
+        #image = image.expand(image.shape[0], TTA, image.shape[2])
+        #print("Step: ",(ii+1)," of ",len(train_dataloader))
+        image = image.to(device)
+        y= y.to(device)
+
+        out = model((image,None))
+        try:
+            clipwise_op = torch.clamp(out["combined_output"],0,1)
+
+            preds = np.argmax(clipwise_op.detach().cpu().squeeze(1).numpy(),axis=1)
+
+            target = np.argmax(y.detach().cpu().squeeze(1).numpy(),axis=1)
+            
+            acc_score += (np.sum(target==preds))/BATCH_SIZE
+            loss = criterion(clipwise_op[:,:,-4:],y[:,:,-4:])
+            #print("LOSS: ",loss.item())
+            acc_score = acc_score/(ii+1)
+            pbar_t.set_postfix(loss=loss.item(),accuracy=acc_score)
+        except Exception as e:
+            print(e)
+
+
+
+
+        loss.backward()
+        optim.step()
+    #print("Train Accuracy: ",acc_score/len(train_dataloader))
+    model.eval()
+    val_acc = 0.0
+    val_preds = []
+    val_targets = []
+    pbar_v = tqdm.tqdm(val_dataloader,unit="batch",leave=False)
+    pbar_v.set_description("Validation")
+    for ii,(x,y) in enumerate(pbar_v):
+        image = x.unsqueeze(1)
+        # image = image.expand(image.shape[0], TTA, image.shape[2])
+        #print("Step: ", (ii + 1), " of ", len(val_dataloader))
+        image = image.to(device)
+        y = y.to(device)
+        with torch.no_grad():
+            out = model((image, None))
+        clipwise_op = torch.clamp(out["combined_output"], 0, 1)
+
+        preds = np.argmax(clipwise_op.detach().cpu().squeeze(1).numpy(), axis=1)
+
+        target = np.argmax(y.detach().cpu().squeeze(1).numpy(), axis=1)
+        val_preds.extend([INV_BIRD_CODE[i] for i in preds.tolist()])
+        val_targets.extend([INV_BIRD_CODE[i] for i in target.tolist()])
+        val_acc += (np.sum(target == preds)) / BATCH_SIZE
+        val_loss = criterion(clipwise_op, y)
+        val_acc = val_acc/(ii+1)
+        #print("Val loss: ",loss.item())
+        pbar_v.set_postfix(loss=val_loss.item(),accuracy=val_acc)
+
+    pred_df = {}
+    pred_df["preds"] = val_preds
+    pred_df["target"] = val_targets
+    pd.DataFrame(pred_df).to_csv("kaggle_1_soln/epoch_"+str(epoch)+"_preds.csv")
+    #print("Val Accuracy: ",val_acc/len(val_dataloader))
+    master_bar.set_postfix(train_loss=loss.item(),val_loss=val_loss.item(),
+                           train_acc=acc_score,val_acc=val_acc)
+
+
+###TESTING####
+#print("Testing....")
+test_bar = tqdm.tqdm(test_dataloader,unit="batch")
+test_bar.set_description("Testing")
+model.eval()
+test_acc = 0.0
+test_preds = []
+test_targets = []
+clip_threshold = 0.2
+for ii,(x,y) in enumerate(test_bar):
+    image = x.unsqueeze(1)
+    #image = image.expand(image.shape[0], TTA, image.shape[2])
+    
+
+    #print("Step: ", (ii + 1), " of ", len(test_dataloader))
+    image = image.to(device)
+    y = y.to(device)
+    with torch.no_grad():
+        out = model((image, None))
+    #clipwise_op = torch.clamp(out["combined_output"], 0, 1)
+    clip_op = out["clipwise_output"].detach().cpu().numpy().mean(axis=1)
+    backbone_op = out["combined_output"].detach().cpu().numpy().mean(axis=1)
+    #bkbone_op = bkbone_op.detach().cpu().numpy()
+
+    clip_indices = np.argsort(-backbone_op,axis=1)
+    clip_indices = clip_indices[:,:1]
+    #clip_thresholded = clip_op >= clip_threshold
+    #backbone_thresholded = backbone_op >= clip_threshold
+    #clip_indices = np.argwhere(clip_thresholded).reshape(-1)
+    #backbone_indices = np.argwhere(backbone_thresholded).reshape(-1)
+
+    #print(backbone_indices)
+    clip_codes = []
+    for ci_arr in clip_indices:
+        inv_codes = []
+        for ci in ci_arr:
+            inv_codes.append(INV_BIRD_CODE[ci])
+        clip_codes.append(inv_codes)
+    test_preds.extend(clip_codes)
+    #print("CC: ",len(clip_codes))
+
+    #args = np.argsort(-bkbone_op.detach().cpu().squeeze(1).numpy(),axis=1)
+    #args = args[:,:5]
+    #preds = test_preds.extend([[INV_BIRD_CODE[j] for j in i] for i in args.tolist()])
+
+
+    #preds = np.argmax(clipwise_op.detach().cpu().squeeze(1).numpy(), axis=1)
+
+    target = np.argmax(y.detach().cpu().squeeze(1).numpy(), axis=1)
+    #print("TT: ", len(target))
+    #test_preds.extend([INV_BIRD_CODE[i] for i in preds.tolist()])
+    test_targets.extend([INV_BIRD_CODE[i] for i in target.tolist()])
+    #test_acc += (np.sum(target == preds)) / BATCH_SIZE
+    #loss = criterion(clipwise_op, y)
+    #print("Test loss: ",loss.item())
+
+
+
+print(test_preds[:10])
+pred_df = {}
+print("Preds: ",len(test_preds))
+print("Targets: ",len(test_targets))
+pred_df["preds"] = test_preds
+pred_df["target"] = test_targets
+pd.DataFrame(pred_df).to_csv("kaggle_1_soln/test_preds.csv")
+#print("Test Accuracy: ",test_acc/len(test_dataloader))
+
+
+
+
+
+
+
+
+#SR = 32000
+#audio,_ = librosa.load("XC_Sounds_Split/yellow-billed_cuckoo/XC1400_0.wav",sr=SR,mono=True,res_type="kaiser_fast")
+#print(len(audio))
+#print(len(np.pad(audio,(0,960000-len(audio)))))'''
+
+
