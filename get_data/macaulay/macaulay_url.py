e052406c7be8da79bda15d8bdb5d404e1ec476ea
aryaman.sriram <aryaman.sriram@gmail.com>
2021-04-13 11:29:14 +0530

    Added macaulay url

---
+++ get_data/macaulay/macaulay_url.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://ebird.org/region/IN/media?yr=all&m="
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content,"html.parser")
+
+name_elements = [element.text for element in soup.find_all("span",class_="Heading-main")]
+
+s = ""
+for name in name_elements[1:]:
+    s+=name+"\n"
+
+with open("macaulay_birds.txt","w+") as f:
+    f.write(s)
