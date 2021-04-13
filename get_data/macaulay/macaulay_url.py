8e1ecb0aa912cf21c2d91c054e6e2348613cdc2b
aryaman.sriram <aryaman.sriram@gmail.com>
2021-04-13 11:29:32 +0530

    Linter checks

--- get_data/macaulay/macaulay_url.py
+++ get_data/macaulay/macaulay_url.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = "https://ebird.org/region/IN/media?yr=all&m="
 
 page = requests.get(url)
-soup = BeautifulSoup(page.content,"html.parser")
+soup = BeautifulSoup(page.content, "html.parser")
 
-name_elements = [element.text for element in soup.find_all("span",class_="Heading-main")]
+name_elements = [element.text for element in soup.find_all("span", class_="Heading-main")]
 
 s = ""
 for name in name_elements[1:]:
-    s+=name+"\n"
+    s += name + "\n"
 
-with open("macaulay_birds.txt","w+") as f:
+with open("macaulay_birds.txt", "w+") as f:
     f.write(s)
