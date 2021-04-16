4fd889a74d9c5f64976f5ef4d9083df063fa84b6
aryaman.sriram <aryaman.sriram@gmail.com>
2021-04-16 10:12:49 +0530

    Made changes

--- get_data/macaulay/macaulay_url.py
+++ get_data/macaulay/macaulay_url.py
@@ -13,10 +13,20 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
 name_elements = [element.text for element in soup.find_all("span", class_="Heading-main")]
+data_species_code = [element["data-species-code"] for element in soup.find_all("a",attrs={"data-species-code":True})]
 
 s = ""
 for name in name_elements[1:]:
     s += name + "\n"
 
+
+with open(save_path, "w+") as f:
+    f.write(s)
+
+s=""
+for code in data_species_code:
+    s+= code+"\n"
+
+save_path = "bird-codes.txt"
 with open(save_path, "w+") as f:
     f.write(s)
