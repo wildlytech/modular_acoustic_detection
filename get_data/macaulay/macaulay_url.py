60c865a596f6c98eb4cb5aa4d15909f4912c50d0
aryaman.sriram <aryaman.sriram@gmail.com>
2021-04-20 10:45:28 +0530

    Removed bird names

--- get_data/macaulay/macaulay_url.py
+++ get_data/macaulay/macaulay_url.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 
-parser = argparse.ArgumentParser(description="Scrapes bird names from argument checklist")
+parser = argparse.ArgumentParser(description="Scrapes bird codes from argument checklist")
 parser.add_argument("-u", "--url", help="URL for checklist", required=True)
-parser.add_argument("-sp", "--save_path", help="Path to save file", required=True)
+parser.add_argument("-sp", "--save_path", help="Path to save file", required=True,default="bird-codes.txt")
 
 args = parser.parse_args()
 url = args.url
@@ -15,17 +15,10 @@ soup = BeautifulSoup(page.content, "html.parser")
 name_elements = [element.text for element in soup.find_all("span", class_="Heading-main")]
 data_species_code = [element["data-species-code"] for element in soup.find_all("a", attrs={"data-species-code": True})]
 
-s = ""
-for name in name_elements[1:]:
-    s += name + "\n"
-
-with open(save_path, "w+") as f:
-    f.write(s)
 
 s = ""
 for code in data_species_code:
     s += code + "\n"
 
-save_path = "bird-codes.txt"
 with open(save_path, "w+") as f:
     f.write(s)
