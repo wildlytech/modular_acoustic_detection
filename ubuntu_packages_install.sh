412d3d3f8f38ba46847f756de00d3730a02be230
Suhaas B <43929346+SuhaasB@users.noreply.github.com>
2021-05-04 05:12:55 +0000

    Update to Dash 1.20 (#49)
    * Changes required Dash version to 1.20.0
    * Removed update-alternatives

--- ubuntu_packages_install.sh
+++ ubuntu_packages_install.sh
@@ -4,8 +4,8 @@
 apt-get install python3.7 python3.7-dev python3.7-distutils python3.7-gdbm python3.7-lib2to3 python3.7-minimal python3.7-tk python3-setuptools python3-pip
 
 # Set default python to python3 (if not already) and default python3 to python 3.7
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+# sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+# sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 
 # Another way to alias python3 but dependencies don't line up
 # echo '' >> ~/.bashrc
